e20db31d61a1d9e5576354141829b808160a996b
AKASH P C <iamakashpc@gmail.com>
2024-02-14 23:16:54 +0530

    backend part complete only remaining is socket.io

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config(); // Move dotenv.config() to the top
 
@@ -9,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 
 app.get("/", (req, res) => {
 	res.send("server running on port " + PORT);
